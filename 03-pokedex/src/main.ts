c57fff0d886d8ba7cf02df919f091b8a4cbf1237
Andres Fuentes <fuentesandres31@gmail.com>
2023-01-26 13:14:18 -0300

    Se configuro el contenido estatico, los global prefix, la configuracion y levantamiento de docker, y la edicion del README.md del proyecto

--- 03-pokedex/src/main.ts
+++ 03-pokedex/src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  //De esta forma el url saldra con api/pokemon...
+  app.setGlobalPrefix('api/v2');
+
   await app.listen(3000);
 }
 bootstrap();
